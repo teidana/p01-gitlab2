741a29725d5901ad5d90015285c240ef29fbaac8
KarolinaZ <wnuczka012@gmail.com>
2020-06-09 23:50:30 +0200

    Czwarty commit

--- 01program.php
+++ 01program.php
@@ -1,6 +1,9 @@
 
-i<?php
+<?php
 echo ("Witaj");
 echo("<br> Jestem programem 01");
-echo ("<br> Test");
+echo ("<br> Test pierwszy");
+echo ("<br> Test drugi");
+echo ("<br> Życzę wszystkim dobrego dnia");
+echo ("<br>Dalsza czesc programu");
 ?>
