2d138dc71bf1b5886e1ce1b938171cdc61536a6b
teidana <66702236+teidana@users.noreply.github.com>
2020-06-10 00:48:21 +0200

    Piąty commit

--- 01program.php
+++ 01program.php
@@ -6,4 +6,6 @@ echo ("<br> Test pierwszy");
 echo ("<br> Test drugi");
 echo ("<br> Życzę wszystkim dobrego dnia");
 echo ("<br>Dalsza czesc programu");
+echo ("<br>Dalsza czesc programu drugiego");
+
 ?>
